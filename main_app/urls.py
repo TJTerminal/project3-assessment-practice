f73cbf9cbf7d25511e18a8e816395c7d9f664194
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-12 19:16:51 -0700

    delete works

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.ToyCreate.as_view(), name='add'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='delete'),
 ]
