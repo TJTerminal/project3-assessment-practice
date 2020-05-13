add938de94acc339d4407af8cab92276033dcd39
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-12 18:46:50 -0700

    add function works now

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Toy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 
 def index(request):
-    # toys = Toy.objects.all()
-    return render(request, 'index.html')
+    toys = Toy.objects.all()
+    return render(request, 'index.html', { 'toys': toys })
 
 class ToyCreate(CreateView):
     model = Toy
     fields = '__all__'
+    success_url = '/'
+
+class ToyDelete(DeleteView):
+    model = Toy
     success_url = '/'
