3b1d8479908caaf517a979f88581aa66c08bac0d
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-12 18:20:59 -0700

    fixed add page

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Toy
+from django.views.generic.edit import CreateView
 
 def index(request):
+    # toys = Toy.objects.all()
     return render(request, 'index.html')
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+    success_url = '/'
